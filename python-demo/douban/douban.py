2420a8a241d24c26590d030012da43219b0288aa
funfish <vvncwvv@yeah.net>
2016-06-24 22:22:41 +0800

    modify python-demo

--- python-demo/douban/douban.py
+++ python-demo/douban/douban.py
@@ -3,7 +3,13 @@
 from urllib.request import urlopen
 import re, string
 import threading, queue, time
+import io, sys  
+from flask import Flask, request, render_template
 
+douban_app = Flask(__name__)
+
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8') #改变标准输出的默认编码  
 content_Q = queue.Queue()
 items = []
 
@@ -35,7 +41,6 @@ def get_title(page):
     titles = re.findall(r'<span class="title">(.*?)</span>', page, re.S)
     for index, item in enumerate(titles):
         if item.find('&nbsp') == -1:
-            print(item)
             items.append(item)
 
 def main():
@@ -51,11 +56,20 @@ def main():
     for thread in threads:
         thread.join()
     content_Q.join()
-    with open("movie.txt", "w+") as movieFile:
-        for item in items:
-            movieFile.write(item + '\n')
+    #with open("movie.txt", "w+") as movieFile:
+    #    for item in items:
+    #        movieFile.write(item + '\n')
+    
     print('End')
+
+
+
+@douban_app.route('/', methods=['GET', 'POST'])
+def home():
+    return 	render_template('home.html', items=items)
 
 if __name__ == '__main__':
     print('Start')
     main()
+    if items:
+    	douban_app.run()
